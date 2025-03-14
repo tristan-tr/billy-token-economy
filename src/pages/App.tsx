0a0fe17354bfe0d9093318c064d5b465b4afca5b
tristan-tr <tristan@durandal.nl>
2025-03-14 13:50:38 +0100

    use tailwind css instead oopsie woopsie :3

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import '../styles/global.scss'
+import '../styles/global.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
