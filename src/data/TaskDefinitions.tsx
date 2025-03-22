c427307d0633c7c6796ce7529ffee306243507fd
tristan-tr <tristan@durandal.nl>
2025-03-22 21:01:31 +0100

    Add extra tasks, budget commented out since probably not necessary yet.

--- src/data/TaskDefinitions.tsx
+++ src/data/TaskDefinitions.tsx
@@ -9,10 +9,17 @@ export const taskDefinitions: TaskDefinition[] = [
         repeatable: true
     },
     {
-        id: 'exercise-task',
+        id: 'lab-exercise-task',
         name: 'Chemistry Experiment',
         description: 'Complete one exercise from the lab assignment.',
         rewardAmount: 7,
+        repeatable: true
+    },
+    {
+        id: 'lecture-task',
+        name: 'Attend a demonstration from the captain',
+        description: 'Watch one multimedia lecture.',
+        rewardAmount: 13,
         repeatable: true
     },
     {
@@ -23,10 +30,38 @@ export const taskDefinitions: TaskDefinition[] = [
         repeatable: true
     },
     {
-        id: 'selfcare-task',
+        id: 'weekly-task',
         name: 'Mend the sails',
         description: 'Do one of your weekly tasks.',
         rewardAmount: 19,
         repeatable: true
+    },
+    {
+        id: 'shower-task',
+        name: 'Washing in the ocean (1x/day)',
+        description: 'Take a shower.',
+        rewardAmount: 13,
+        repeatable: true
+    },
+    {
+        id: 'dishes-task',
+        name: 'Wash the dishes',
+        description: 'Do 5 dishes in the kitchen. Everything but utensils counts.',
+        rewardAmount: 7,
+        repeatable: true
+    },
+    {
+        id: 'clean-room-task',
+        name: 'Sweep the deck (1x/day)',
+        description: 'Tidy your room.',
+        rewardAmount: 7,
+        repeatable: true
     }
+    // {
+    //     id: 'budget-task',
+    //     name: "Quartermaster's Financial Duties (1x/day)",
+    //     description: 'Look at and fix your budget.',
+    //     rewardAmount: 3,
+    //     repeatable: true
+    // }
 ];
