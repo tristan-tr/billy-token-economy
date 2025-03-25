aa4eb959794f7e2415a56ebeca736255286bbbf4
tristan-tr <tristan@durandal.nl>
2025-03-25 20:35:03 +0100

    migration script

---
+++ src/migrations/001_convert_completed_tasks_to_array.ts
@@ -0,0 +1,5 @@
+export default async function migrate() {
+    // clear completed tasks, has no useful data as of now
+    localStorage.setItem('stored-tasks', '[]');
+    console.log('Migration 001_convert_completed_tasks_to_array completed');
+}
